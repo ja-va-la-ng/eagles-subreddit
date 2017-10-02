0fda5e7015eac0e810cbfcbb06cfd027ac817192
Matthew Evert <hello.mevert@gmail.com>
2017-10-02 13:18:55 -0400

    Misc style changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
