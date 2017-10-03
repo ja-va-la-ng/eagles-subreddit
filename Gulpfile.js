4053feb88834a142d79a1aa92008ee0575f619d1
Matthew Evert <hello.mevert@gmail.com>
2017-10-03 08:15:16 -0400

    Misc changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/eagles.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
@@ -11,3 +11,5 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+// outputStyle: 'compressed'
